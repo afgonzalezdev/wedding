f33a78690cecc3fc9948dbabc7739ab3e5897af8
FF-agonzalez <agonzalez@forte.io>
2023-08-22 20:16:30 -0300

    change effect

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,9 +21,35 @@ document.addEventListener("DOMContentLoaded", function() {
     const targetElement = document.getElementById('wedding-header');
     
     if (targetElement) {
-      targetElement.scrollIntoView({
-        behavior: 'smooth'
-      });
+      smoothScrollTo(targetElement, 2000); // Scroll over 2 seconds (2000ms)
     }
-  });
-  
+});
+
+function smoothScrollTo(target, duration) {
+    let start = window.pageYOffset || document.documentElement.scrollTop,
+        change = target.offsetTop - start,
+        startTime = Date.now(),
+        val;
+
+    const animateScroll = function() {
+        const currentTime = Date.now() - startTime;
+        val = Math.easeInOutQuad(currentTime, start, change, duration);
+
+        window.scrollTo(0, val);
+
+        if (currentTime < duration) {
+            requestAnimationFrame(animateScroll);
+        } else {
+            window.scrollTo(0, target.offsetTop);
+        }
+    };
+
+    requestAnimationFrame(animateScroll);
+}
+
+Math.easeInOutQuad = function (t, b, c, d) {
+  t /= d / 2;
+  if (t < 1) return c / 2 * t * t + b;
+  t--;
+  return -c / 2 * (t * (t - 2) - 1) + b;
+};
