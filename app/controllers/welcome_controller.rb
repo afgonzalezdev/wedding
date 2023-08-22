d61ee66bfbdf5a8aae769194ba1864538a730377
FF-agonzalez <agonzalez@forte.io>
2023-08-21 21:42:06 -0300

    ic

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class WelcomeController < ApplicationController
+  def index
+    @google_maps_url = 'https://goo.gl/maps/Cd9kNRrWZcqsdZok6'
+    @party_url = 'https://goo.gl/maps/8XQ533kjxabYfh2x5'
+
+    @cool_earth_url = 'https://www.coolearth.org/'
+  end
+end
